7da94765de90c39829425bbbd26bc0c83c3662b0
David nguyễn <dinhdat2104@gmail.com>
2026-05-29 12:47:25 +0700

    model đã được định dạng

--- courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/model/dto/FineTuningDto.java
+++ courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/model/dto/FineTuningDto.java
@@ -1,8 +1,20 @@
 package com.courseqa.model.dto;
 
+import java.util.UUID;
+
 // DTOs for fine-tuning dataset export and experiment tracking.
 // TODO: Add request/response DTO classes here.
 
 public class FineTuningDto {
+public static class ExportResponse {
+        public UUID datasetId;
+        public String filePath;
+        public Integer totalLines;
 
+        public ExportResponse(UUID datasetId, String filePath, Integer totalLines) {
+            this.datasetId = datasetId;
+            this.filePath = filePath;
+            this.totalLines = totalLines;
+        }
+    }
 }
