f77b4ea021e913e4ef4ac25344959599fb5244fc
Nekostream <94790421+MiKhangz@users.noreply.github.com>
2026-06-08 20:59:14 +0700

    CourseWorkspaceRepository

--- courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/CourseWorkspaceRepository.java
+++ courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/CourseWorkspaceRepository.java
@@ -1,9 +1,18 @@
 package com.courseqa.repository;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.courseqa.model.entity.CourseWorkspace;
+
 // TODO: Extend JpaRepository after completing entity class.
 // Example:
 // public interface CourseWorkspaceRepository extends JpaRepository<CourseWorkspace, UUID> {}
 
-public interface CourseWorkspaceRepository {
-
+public interface CourseWorkspaceRepository extends JpaRepository<CourseWorkspace, UUID> {
+List<CourseWorkspace> findByCourseId(UUID courseId);
+List<CourseWorkspace> findByOwnerUserId(UUID userId);
+boolean existsByCourseIdAndWorkspaceTitle(UUID courseId, String workspaceTitle);
 }
