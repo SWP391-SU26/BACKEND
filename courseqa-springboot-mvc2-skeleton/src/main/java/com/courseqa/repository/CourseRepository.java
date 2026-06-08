050aa7d67bbcb311eacb48a907ea9653fae2651f
Nekostream <94790421+MiKhangz@users.noreply.github.com>
2026-06-08 20:40:29 +0700

    CourseRepository

--- courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/CourseRepository.java
+++ courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/CourseRepository.java
@@ -1,9 +1,18 @@
 package com.courseqa.repository;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.courseqa.model.entity.Course;
+
 // TODO: Extend JpaRepository after completing entity class.
 // Example:
 // public interface CourseRepository extends JpaRepository<Course, UUID> {}
 
-public interface CourseRepository {
-
+public interface CourseRepository extends JpaRepository<Course, UUID> {
+List<Course> findByIsActiveTrue();
+List<Course> findByCreatedBy(UUID createdBy);
+boolean existsByCourseCode(String courseCode);
 }
--- courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/UserRoleRepository.java
+++ courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/UserRoleRepository.java
@@ -1,9 +1,17 @@
 package com.courseqa.repository;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.courseqa.model.entity.UserRole;
+
 // TODO: Extend JpaRepository after completing entity class.
 // Example:
 // public interface UserRoleRepository extends JpaRepository<UserRole, UUID> {}
 
-public interface UserRoleRepository {
-
+public interface UserRoleRepository extends JpaRepository<UserRole,UUID> {
+List<UserRole> findByUserId(UUID userId);
+boolean existsByUserIdAndRoleId(UUID userId, UUID roleId);
 }
