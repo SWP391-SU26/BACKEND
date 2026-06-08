149942b77872d8ccc4bd94797be9709c31945ae0
Nekostream <94790421+MiKhangz@users.noreply.github.com>
2026-06-08 16:57:58 +0700

    repository update
    UserRepository

--- courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/UserRepository.java
+++ courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package com.courseqa.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.courseqa.model.entity.User;
+
 // TODO: Extend JpaRepository after completing entity class.
 // Example:
 // public interface UserRepository extends JpaRepository<User, UUID> {}
-
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+Optional<User> findByEmail(String email);
+boolean existsByEmail(String email);
 
 }
