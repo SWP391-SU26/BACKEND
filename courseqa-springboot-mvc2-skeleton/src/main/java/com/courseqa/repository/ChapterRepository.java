34e5e79533f0125e3e2da9884c0d725fc9e2e51f
Nekostream <94790421+MiKhangz@users.noreply.github.com>
2026-06-08 20:46:55 +0700

    ChapterRepository

--- courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/ChapterRepository.java
+++ courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/repository/ChapterRepository.java
@@ -1,9 +1,17 @@
 package com.courseqa.repository;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.courseqa.model.entity.Chapter;
+
 // TODO: Extend JpaRepository after completing entity class.
 // Example:
 // public interface ChapterRepository extends JpaRepository<Chapter, UUID> {}
 
-public interface ChapterRepository {
-
+public interface ChapterRepository extends JpaRepository<Chapter, UUID> {
+List<Chapter> findByCourseIdOrderByOrderIndexAsc(UUID courseId);
+boolean existsByCourseIdAndChapterTitle(UUID courseId, String chapterTitle);
 }
