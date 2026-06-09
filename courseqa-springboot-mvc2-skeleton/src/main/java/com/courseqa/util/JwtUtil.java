dd6b2838293a4bef08a2702966f8b6e2e0490d3e
Nekostream <94790421+MiKhangz@users.noreply.github.com>
2026-06-09 15:52:07 +0700

    JwtUtil
    added util, new utility added

---
+++ courseqa-springboot-mvc2-skeleton/src/main/java/com/courseqa/util/JwtUtil.java
@@ -0,0 +1,53 @@
+package com.courseqa.util;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtUtil {
+@Value("${app.jwt.secret}")
+private String secret;
+
+@Value("${app.jwt.expiration-ms}")
+private long expirationMs;
+
+private Key getSigningKey() {
+   
+    return Keys.hmacShaKeyFor(secret.getBytes());
+}
+
+public String generateToken(String email){
+    return Jwts.builder()
+    .setSubject(email)
+    .setIssuedAt(new Date())
+    .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+    .signWith(getSigningKey(),SignatureAlgorithm.HS256)
+    .compact();
+}
+
+private Claims extractClaims(String token){
+    return Jwts.parserBuilder()
+    .setSigningKey(getSigningKey())
+    .build().parseClaimsJws(token)
+    .getBody();
+}
+
+ public String extractEmail(String token){
+    return extractClaims(token).getSubject();
+ }
+
+public boolean isTokenValid(String token){
+    try {
+      return !extractClaims(token).getExpiration().before(new Date());
+    }catch (Exception e) {
+        return false;
+    }
+
+}
+}
