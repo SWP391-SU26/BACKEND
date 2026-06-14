14c67c6579bdfca2f065123e8fab51e90b6a14cf
Nekostream <94790421+MiKhangz@users.noreply.github.com>
2026-06-14 13:17:03 +0700

    fix: add WebClient bean to AppConfig

--- courseqa-springboot-mvc2-skeleton/BACKEND/src/main/java/com/courseqa/config/AppConfig.java
+++ courseqa-springboot-mvc2-skeleton/BACKEND/src/main/java/com/courseqa/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.reactive.function.client.WebClient;
+
 @Configuration
 public class AppConfig {
     @Bean
